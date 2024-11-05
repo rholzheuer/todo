7e2ff70a5d9ec56422d47be9c22075626c4a5d6a
Robin Holzheuer <robin.holzheuer@student.hnu.de>
2024-11-05 18:22:01 +0200

    first commit

---
+++ server/routes/todoRouter.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import {
+  getTasks,
+  postTask,
+  taskDelete,
+} from "../controllers/TaskController.js";
+
+const router = Router();
+
+router.get("/", getTasks);
+
+router.post("/create", postTask);
+
+router.delete("/delete/:id", taskDelete);
+
+export default router;
